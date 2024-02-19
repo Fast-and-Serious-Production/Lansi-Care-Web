934ac4638d3924cecac30b220cd69d8fc2c029d5
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-02-19 21:43:56 +0700

    feat: add signin FE

---
+++ src/app/users/component/SigninUser.tsx
@@ -0,0 +1,6 @@
+"use client";
+import React from "react";
+
+export default function SigninUser() {
+  return <div>SigninUser</div>;
+}
--- src/app/users/signup/page.tsx
+++ src/app/users/signup/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import SignUpUser from "../component/SignUpUser";
 
 export default function signup() {
-  return <div>signup</div>;
+  return (
+    <div className="w-screen h-screen">
+      <SignUpUser />
+    </div>
+  );
 }
