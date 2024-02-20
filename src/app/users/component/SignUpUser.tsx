0d14e325554f9950c8334781cfd0172b39c1bb33
khakhaaa <rakha_adrian_32rpl@student.smktelkom-mlg.sch.id>
2024-02-20 08:10:16 +0700

    about

--- src/app/users/component/SignUpUser.tsx
+++ src/app/users/component/SignUpUser.tsx
@@ -7,8 +7,8 @@ import Image from "next/image";
 export default function SignUpUser() {
   return (
     <>
-      <section className="flex flex-1 flex-col min-w-full justify-center lg:px-8 align-middle h-full mt-20">
-        <section className=" max-w-7xl flex justify-around max-h-3xl m-auto w-full h-fit pb-12 pt-5 rounded-2xl shadow-sm shadow-slate-600">
+      <section className="flex flex-1 flex-col min-w-full justify-center lg:px-8 align-middle h-full mt-20 bg-base-50">
+        <section className=" max-w-7xl flex justify-around max-h-3xl m-auto w-full h-fit pb-12 pt-5 rounded-3xl shadow-md shadow-slate-600 bg-primary-1000">
           <main>
             <Image src={SignupImage} width={397.22} height={305.61} alt="SignUp Image" />
           </main>
