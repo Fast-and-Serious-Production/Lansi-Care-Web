d70aba2864548403c8fecf05ae211cb782f32d4e
khakhaaa <rakha_adrian_32rpl@student.smktelkom-mlg.sch.id>
2024-02-21 09:31:36 +0700

    home lagi

--- src/app/users/component/HomePage.tsx
+++ src/app/users/component/HomePage.tsx
@@ -18,15 +18,24 @@ export default function Homepage() {
             <div className="w-96 h-6 px-10 mt-[33px] flex-col justify-start items-start inline-flex">
               <div className="self-stretch text-neutral-800 text-xl font-semibold leading-normal">Fitur-fitur</div>
             </div>
-            <div className="w-20 h-5 pl-2 pr-1 py-0.5 rounded border border-neutral-800 justify-start items-start inline-flex flex-col">
-              <button className="text-neutral-800 text-xs font-normal">See Details</button>
+            <div className="relative">
+            <div className="w-[86px] h-[22px] pl-2 pr-1 py-0.5 rounded border border-neutral-800 justify-start items-start absolute top-0 right-[45px] inline-flex flex-col">
+              <button className="text-neutral-800 text-xs font-normal ">See Details</button>
               <div className="w-3 h-3 relative" />
+            </div>
             </div>
           </div>
 
           <div className="w-[1366.48px] h-[233.31px] mt-[20px] mx-[35] bg-slate-50 rounded-[10px] shadow-md">
             <div className="w-96 h-6 px-10 mt-[33px] flex-col justify-start items-start inline-flex">
               <div className="self-stretch text-neutral-800 text-xl font-semibold leading-normal">Komentar</div>
+            </div>
+
+            <div className="relative">
+            <div className="w-[105px] h-[22px] pl-2 pr-1 py-0.5 rounded border border-neutral-800 justify-start items-start absolute top-0 right-[45px] inline-flex flex-col">
+              <button className="text-neutral-800 text-xs font-normal ">Write a Review</button>
+              <div className="w-3 h-3 relative" />
+            </div>
             </div>
           </div>
 
