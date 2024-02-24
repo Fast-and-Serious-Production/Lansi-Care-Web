a781369d89da5b1011ad8ddece2e7c0bc0d7bf21
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-02-24 13:05:09 +0700

    p

--- src/app/users/component/AboutComponent.tsx
+++ src/app/users/component/AboutComponent.tsx
@@ -7,61 +7,108 @@ import Image from "next/image";
 export default function AboutComponent() {
   return (
     <>
-    <section>
-      <div className="mt-40 mx-auto text-base-100">
-        <h6 className="text-center m-1 text-xl">About us</h6>
-        <h1 className="mb-20 mt-10 text-center font-bold text-6xl mx-80">InnoSync</h1>
-      </div>
+      <section>
+        <div className="mx-auto mt-40 text-base-100">
+          <h6 className="m-1 text-center text-xl">About us</h6>
+          <h1 className="mx-80 mb-20 mt-10 text-center text-6xl font-bold">
+            InnoSync
+          </h1>
+        </div>
 
-      <div className="relative mb-20 mt-[400px] w-full h-10 grid grid-cols-4">
-        <div className="relative flex justify-center">
-          <div className="absolute w-52 -top-64 hover:scale-105 transition-all group">
-            <Image src={image1} alt="image" className="rounded-[6px] group-hover:ring-4 group-hover:ring-slate-300" />
-            <p className="text-base font-medium hidden group-hover:block text-center mt-3">Muhammad Keefa Syawal (Hipster)</p>
+        <div className="relative mb-20 mt-[400px] grid h-10 w-full grid-cols-4">
+          <div className="relative flex justify-center">
+            <div className="group absolute -top-64 w-52 transition-all hover:scale-105">
+              <Image
+                src={image1}
+                alt="image"
+                className="rounded-[6px] group-hover:ring-4 group-hover:ring-slate-300"
+              />
+              <p className="mt-3 hidden text-center text-base font-medium group-hover:block">
+                Muhammad Keefa Syawal (Hipster)
+              </p>
+            </div>
+          </div>
+          <div className="relative flex justify-center">
+            <div className="group absolute -top-32 w-52 transition-all hover:scale-105">
+              <Image
+                src={image2}
+                alt="image"
+                className="rounded-[6px] group-hover:ring-4 group-hover:ring-slate-300"
+              />
+              <p className="mt-3 hidden text-center text-base font-medium group-hover:block">
+                Naufal Nabil Ramadhan (Hacker)
+              </p>
+            </div>
+          </div>
+          <div className="relative flex justify-center">
+            <div className="group absolute -top-64 w-52 transition-all hover:scale-105">
+              <Image
+                src={image3}
+                alt="image"
+                className="rounded-[6px] group-hover:ring-4 group-hover:ring-slate-300"
+              />
+              <p className="mt-3 hidden text-center text-base font-medium group-hover:block">
+                Raisya Ramadhani Achmad (Husler)
+              </p>
+            </div>
+          </div>
+          <div className="relative flex justify-center">
+            <div className="group absolute -top-32 w-52  transition-all hover:scale-105">
+              <Image
+                src={image4}
+                alt="image"
+                className="rounded-[6px] group-hover:ring-4 group-hover:ring-slate-300"
+              />
+              <p className="mt-3 hidden text-center text-base font-medium group-hover:block">
+                Rakha Adrian Nur Tanaya (Leader)
+              </p>
+            </div>
           </div>
         </div>
-        <div className="relative flex justify-center">
-          <div className="absolute w-52 -top-32 hover:scale-105 transition-all group">
-            <Image src={image2} alt="image" className="rounded-[6px] group-hover:ring-4 group-hover:ring-slate-300" />
-            <p className="text-base font-medium hidden group-hover:block text-center mt-3">Naufal Nabil Ramadhan (Hacker)</p>
-          </div>
-        </div>
-        <div className="relative flex justify-center">
-          <div className="absolute w-52 -top-64 hover:scale-105 transition-all group">
-            <Image src={image3} alt="image" className="rounded-[6px] group-hover:ring-4 group-hover:ring-slate-300" />
-            <p className="text-base font-medium hidden group-hover:block text-center mt-3">Raisya Ramadhani Achmad (Husler)</p>
-          </div>
-        </div>
-        <div className="relative flex justify-center">
-          <div className="absolute -top-32 w-52 hover:scale-105  transition-all group">
-            <Image src={image4} alt="image" className="rounded-[6px] group-hover:ring-4 group-hover:ring-slate-300" />
-            <p className="text-base font-medium hidden group-hover:block text-center mt-3">Rakha Adrian Nur Tanaya (Leader)</p>
-          </div>
-        </div>
-      </div>
 
-      {/* deskripsi */}
-      <div className="relative mx-40 h-full my-40">
-        <div className="mt-10 ml-20">
-          <div className="">
-            <h1 className="font-bold text-5xl flex justify-start">Who</h1>
-            <h3 className="break-words font-bold text-5xl text-darkBlue ml-5">are we</h3>
-          </div>
-          <div className="font-medium">
-            <p className="text-left flex justify-normal mr-20 ml-80">
-            Selamat datang di website resmi LansiCare! Kami adalah tim Lorem yang terdiri dari empat individu berbakat yang terdiri dari Rakha, sebagai ketua kami, memimpin dengan visi yang kuat serta mengarahkan tim untuk terus berkembang. Ada pula hacker kami yaitu Naufal, membawa keahlian teknis yang mendalam serta bertanggung jawab atas program yang ada. Serta ada Raisya, hustler kami, memiki keterampilan komunikasi dan negosiasi yang luar biasa, dan ada pula Keefa, hipster kreatif, menambahkan sentuhan estetika yang unik, sehingga membawa kesan megah kepada tim kami. 
-            </p>
-            <br />
-            <p className="text-left flex justify-normal mr-20 ml-80">
-            Kami menyediakan platform untuk memudahkan lansia dalam menjalani check-up kesehatan. Website ini dirancang untuk memberikan bantuan dengan melakukan registrasi check-up secara online sehingga mereka tidak perlu mengantre di tempat. Selain itu, kami juga menyediakan fitur dimana lansia bisa mengecek riwayat kesehatan sehingga mempermudah mereka untuk mengakses informasi kesehatan. Dengan adanya website ini, kami berharap bisa memberikan alternatif atas permasalahan yang ada, sehingga website kami memiliki manfaat bagi lingkungan sekitar.
-            </p>
-            <br />
-            <p className="text-left flex justify-normal mr-20 ml-80">
-            Kami bersatu untuk menciptakan solusi luar biasa dan menghadirkan ide-ide kreatif. Terima kasih telah berkunjung ke situs kami.
-            </p>
+        {/* deskripsi */}
+        <div className="relative mx-40 my-40 h-full">
+          <div className="ml-20 mt-10">
+            <div className="">
+              <h1 className="flex justify-start text-5xl font-bold">Who</h1>
+              <h3 className="ml-5 break-words text-5xl font-bold text-darkBlue">
+                are we
+              </h3>
+            </div>
+            <div className="font-medium">
+              <p className="ml-80 mr-20 flex justify-normal text-left">
+                Selamat datang di website resmi LansiCare! Kami adalah tim
+                InnoSync yang terdiri dari empat individu berbakat yang terdiri
+                dari Rakha, sebagai ketua kami, memimpin dengan visi yang kuat
+                serta mengarahkan tim untuk terus berkembang. Ada pula hacker
+                kami yaitu Naufal, developer yang membawa keahlian teknis yang
+                mendalam serta bertanggung jawab atas program yang ada. Serta
+                ada Raisya, hustler kami, memiki keterampilan komunikasi dan
+                negosiasi yang luar biasa, dan ada pula Keefa, hipster kreatif,
+                menambahkan sentuhan estetika yang unik, sehingga membawa kesan
+                megah kepada tim kami.
+              </p>
+              <br />
+              <p className="ml-80 mr-20 flex justify-normal text-left">
+                Kami menyediakan platform untuk memudahkan lansia dalam
+                menjalani check-up kesehatan. Website ini dirancang untuk
+                memberikan bantuan dengan melakukan registrasi check-up secara
+                online sehingga mereka tidak perlu mengantre di tempat. Selain
+                itu, kami juga menyediakan fitur dimana lansia bisa mengecek
+                riwayat kesehatan sehingga mempermudah mereka untuk mengakses
+                informasi kesehatan. Dengan adanya website ini, kami berharap
+                bisa memberikan alternatif atas permasalahan yang ada, sehingga
+                website kami memiliki manfaat bagi lingkungan sekitar.
+              </p>
+              <br />
+              <p className="ml-80 mr-20 flex justify-normal text-left">
+                Kami bersatu untuk menciptakan solusi luar biasa dan
+                menghadirkan ide-ide kreatif. Terima kasih telah berkunjung ke
+                situs kami.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
       </section>
     </>
   );
